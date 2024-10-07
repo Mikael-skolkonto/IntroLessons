673ba8faa85e585515644b6cb6546530e8445347
Mikael-skolkonto <mikael.overfjord06@skola.stenungsund.se>
2024-10-08 01:03:11 +0200

    Löste problem 45 från project euler

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import ProgrammeringsUppgifter.RockPaperScissors;
 
 public class Main {
     public static void main(String[] args) {
-        //new RockPaperScissors();
         new Problem45();
     }
 }
